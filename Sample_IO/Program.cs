fd435f94906bbfc8e11d0edc77e74e54ce9e78b2
squallcloud <squallcloudzidane@gmail.com>
2018-09-30 23:50:36 +0900

    feat: フォルダ内を探索するプログラムを実装

--- Sample_IO/Program.cs
+++ Sample_IO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,42 @@ namespace Sample_IO
 {
 	class Program
 	{
+		private static void SearchFile(string rootPath, Func<string, bool> func)
+		{
+			try {
+				var files = Directory.GetFiles(rootPath);
+				foreach (var file in files) {
+					func(file);
+				}
+			} catch (UnauthorizedAccessException ex) {
+				Console.Error.WriteLine(ex.Message);
+			} finally {
+			}
+
+			try {
+				var dirs = Directory.GetDirectories(rootPath);
+				foreach (var dir in dirs) {
+					SearchFile(dir, func);
+				}
+			} catch (UnauthorizedAccessException ex) {
+				Console.Error.WriteLine(ex.Message);
+			} finally {
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			try {
+				string rootPath = "C:/";
+				SearchFile(rootPath, x => {
+					Console.WriteLine(x);
+					return true;
+				});
+			} catch (Exception ex) {
+				Console.Error.WriteLine(ex.Message);
+			} finally {
+
+			}
 		}
 	}
 }
